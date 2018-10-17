d65fe99c7057aad422be597b744bc6c6f520b697
Rizwan Sharif <rizwan_sharif@hotmail.com>
2018-10-17 11:26:49 +0100

    Explained the functions

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400, 400)
+  background(25)
+}
+// This changes the canvas size and the colour of the background.
+function draw() {
+  fill(255, 130, 0)
+  stroke(0)
+  triangle(0, 30, 40, 0, 80, 30)
+  rect(5, 30, 70, 70)
+}
+// The draw function changes the colour of the shapes and the sizes.
