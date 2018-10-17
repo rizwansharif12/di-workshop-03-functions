0ec9b39fbbcb5753aef4165fd845b6db63ce666c
Rizwan Sharif <rizwan_sharif@hotmail.com>
2018-10-17 14:41:28 +0100

    Made house move with mouse pointer

--- sketch.js
+++ sketch.js
@@ -1,14 +1,16 @@
 function setup() {
   createCanvas(400, 400)
-  background(255)
 }
 // This changes the canvas size and the colour of the background.
 
 function draw() {
+
   fill(255, 130, 0)
   stroke(0)
-  roof (0, 30, 40, 0, 80, 30)
-  house (5, 30, 70)
+  background(255)
+  roof(mouseX, mouseY+30, mouseX+40, mouseY, mouseX+80, mouseY+30)
+  square(mouseX+5, mouseY+30, 70)
+
 }
 // The draw function changes the colour of the shapes and the sizes.
 
@@ -18,10 +20,5 @@ function square(x, y, size) {
 
 function roof(x1, y1, x2, y2, x3, y3) {
   triangle(x1, y1, x2, y2, x3, y3)
-  // ...
-}
-
-function house(x, y, size) {
-  square(x, y, size)
   // ...
 }
