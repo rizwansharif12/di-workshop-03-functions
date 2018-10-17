d8ca1656e15fbeaee2b3cf1ecf4581a9d18e0171
Rizwan Sharif <rizwan_sharif@hotmail.com>
2018-10-17 14:11:26 +0100

    Changed the house

--- sketch.js
+++ sketch.js
@@ -1,12 +1,27 @@
 function setup() {
   createCanvas(400, 400)
-  background(25)
+  background(255)
 }
 // This changes the canvas size and the colour of the background.
+
 function draw() {
   fill(255, 130, 0)
   stroke(0)
-  triangle(0, 30, 40, 0, 80, 30)
-  rect(5, 30, 70, 70)
+  roof (0, 30, 40, 0, 80, 30)
+  house (5, 30, 70)
 }
 // The draw function changes the colour of the shapes and the sizes.
+
+function square(x, y, size) {
+  rect(x, y, size, size)
+}
+
+function roof(x1, y1, x2, y2, x3, y3) {
+  triangle(x1, y1, x2, y2, x3, y3)
+  // ...
+}
+
+function house(x, y, size) {
+  square(x, y, size)
+  // ...
+}
