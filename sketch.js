6733be81c9174c1ab44f6463e7d7d0fc1910f1aa
Rizwan Sharif <rizwan_sharif@hotmail.com>
2018-10-18 11:05:10 +0100

    Made three houses next to each other

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 function setup() {
-  createCanvas(400, 400)
+  createCanvas(600, 600)
 }
 // This changes the canvas size and the colour of the background.
 
@@ -7,9 +7,13 @@ function draw() {
 
   fill(255, 130, 0)
   stroke(0)
-  background(255)
+  background(0)
   roof(mouseX, mouseY+30, mouseX+40, mouseY, mouseX+80, mouseY+30)
   square(mouseX+5, mouseY+30, 70)
+  roof(mouseX+80, mouseY+30, mouseX+120, mouseY, mouseX+160, mouseY+30)
+  square(mouseX+86, mouseY+30, 70)
+  roof(mouseX+160, mouseY+30, mouseX+200, mouseY, mouseX+240, mouseY+30)
+  square(mouseX+165, mouseY+30, 70)
 
 }
 // The draw function changes the colour of the shapes and the sizes.
@@ -17,6 +21,7 @@ function draw() {
 function square(x, y, size) {
   rect(x, y, size, size)
 }
+
 
 function roof(x1, y1, x2, y2, x3, y3) {
   triangle(x1, y1, x2, y2, x3, y3)
